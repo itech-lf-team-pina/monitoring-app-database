df4a4fc53f31e5acb61c242568fbe1916f153cbe
Adrian <adrian@ganseforth.me>
2022-12-13 10:50:01 +0100

    add creation skript for tables

---
+++ lf8_create.sql
@@ -0,0 +1,38 @@
+create table if not exists hardware_type
+(
+    id   int auto_increment
+        primary key,
+    name text not null
+)
+    comment 'Typ der Messung';
+
+create table if not exists hardware
+(
+    id            int auto_increment
+        primary key,
+    value         int null,
+    hardware_type int null,
+    constraint hardware_hardware_type_fk
+        foreign key (hardware_type) references hardware_type (id)
+)
+    comment 'Tabelle für Messdaten';
+
+create table if not exists log
+(
+    id        bigint unsigned auto_increment
+        primary key,
+    log       text                                  not null,
+    timestamp timestamp default current_timestamp() not null,
+    constraint id
+        unique (id)
+);
+
+create table if not exists log_type
+(
+    id   int auto_increment
+        primary key,
+    name text not null
+)
+    comment 'Typ des Logeintrags';
+
+
